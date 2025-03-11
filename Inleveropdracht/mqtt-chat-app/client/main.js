92e2266c6f2984137d33a9ca9726c052380477c7
ivobr <ivobruinsma23@gmail.com>
2025-03-11 12:27:48 +0100

    FUCKKKKKKKKKKKKKK

--- Inleveropdracht/mqtt-chat-app/client/main.js
+++ Inleveropdracht/mqtt-chat-app/client/main.js
@@ -1,32 +1,48 @@
-const client = mqtt.connect('ws://localhost:9001'); //websocket port
 
-const Userid = Math.floor(Math.random() * 1000);
+    // const broker = "ws://localhost:9001"; //websocket port
+    const broker = "ws://host.docker.internal:9001";
 
-// when client gets connection to page
-client.on('connect', () => { 
-    console.log("connected");
-    document.getElementById('connection').innerText ='connected'; //stuur opgevraagde
-    client.subscribe('chat');
-});
 
-//getting the message
-client.on('message', (topic, message) => {
-    const chat = document.getElementById('chat');
-    const msg = document.createElement('div');
-    msg.textContent = message.toString();
-    chat.appendChild(msg);
-});
+    const options = {
+        username: "admin",
+        password: "admin",
+    }
+    const topic = "chatroom";
 
-//sending the message
-function sendMessage(){
-    const message = document.getElementById('message').value;
-    client.publish('chat', message);
-    document.getElementById('message').value= '';
-    console.log(Userid + " " + message);
-}
+    const client = mqtt.connect(broker, options);
 
-function send(event){
-    event.preventDefault();
-    var data = document.getElementById("data");
-    console.log(data.value);
-}
+    // const client = mqtt.connect(broker);
+
+    client.on('error', (error) =>{
+        console.error("Connection error: ", error);
+    });
+
+    // when client gets connection to page
+    client.on('connect', () => { 
+
+        client.subscribe(topic, (err) =>{
+            if(!err){
+                document.getElementById('connection').innerText ='connected'; //stuur opgevraagde
+            } 
+        });
+    });
+
+    //getting the message
+    client.on('message', (topic, message) => {
+        const chat = document.getElementById('chat');
+        const msg = document.createElement('div');
+        msg.textContent = message.toString();
+        chat.appendChild(msg);
+    });
+
+
+
+    function send(event){
+        event.preventDefault();
+        var data = document.getElementById("data");
+        client.publish(topic, data.value);
+        // client.end();
+        console.log(data.value);
+        
+        data.value="";
+    }
