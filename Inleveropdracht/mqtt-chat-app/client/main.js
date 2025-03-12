6b94db2786ffd1c049fdc112b67508aeca14780d
ivobr <ivobruinsma23@gmail.com>
2025-03-12 12:57:13 +0100

    fuck this shit man

--- Inleveropdracht/mqtt-chat-app/client/main.js
+++ Inleveropdracht/mqtt-chat-app/client/main.js
@@ -1,21 +1,33 @@
+let client;
+let topic = "chatroom";
+let username;
+    
+function login(event){
+    event.preventDefault();
+
+    username = document.getElementById("Username").value;
+    const password = document.getElementById("Password").value;
+
+    if( !username || !password){
+        document.getElementById('Login-error').innerText = 'Alsjeblieft vul gewoon alles in idioot';
+        return;
+    }
 
     const broker = "ws://localhost:1884"; //websocket port
-    // const broker = "ws://host.docker.internal:9001";
-    const username = Math.floor(Math.random() * 1000);
-
-
     const options = {
-        username: "admin",
-        password: "admin",
+        username: username,
+        password: password,
     }
-    const topic = "chatroom";
+ 
 
-    const client = mqtt.connect(broker, options);
+
+    client = mqtt.connect(broker, options);
 
     // const client = mqtt.connect(broker);
 
     client.on('error', (error) =>{
         console.error("Connection error: ", error);
+        document.getElementById('Login-error').innerText = 'Log in Failed';
     });
 
     // when client gets connection to page
@@ -24,7 +36,16 @@
         client.subscribe(topic, (err) =>{
             if(!err){
                 document.getElementById('connection').innerText ='connected'; //stuur opgevraagde
-            } 
+
+                const JoinMessage = username + ": joind the chat." 
+                client.publish(topic, JoinMessage);
+
+                localStorage.setItem('username', username);
+                localStorage.getItem('broker', broker);
+                window.location.href = 'ChatRoom.html';
+            } else{
+                
+            }
         });
     });
 
@@ -35,16 +56,16 @@
         msg.textContent = message.toString();
         chat.appendChild(msg);
     });
-
+}
 
 
     function send(event){
         event.preventDefault();
-        var data = document.getElementById("data");
-        var dataSend = (username + ": " + data.value);
-        client.publish(topic, dataSend);
-        // client.end();
-        console.log(dataSend);
+        const message = document.getElementById('data').value;
+        console.log(username);
+
         
-        data.value="";
+        const dataSend = `${username}: ${message}`;
+        client.publish(topic, dataSend);
+        document.getElementById('data').value = '';
     }
